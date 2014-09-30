c6087dacf31a4e578ff29a9894ea432f37ac1af2
Chris Pennycuick <christopher.pennycuick@gmail.com>
2014-09-30 16:51:47 +1000

    fix db query

--- framework/tools/database.php
+++ framework/tools/database.php
@@ -1,44 +1,44 @@
 <?php
 
 class DataBase extends Tool {
-	
+
 	private $connections = [];
 	private $defaultConnection = null;
-	
+
 	public function openConnection($dsn, $username = null, $password = null, $options = null, $name = null) {
 		$con = new DataBaseConnection($dsn, $username, $password, $options);
 		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
+
 		if (!$this->defaultConnection) {
 			$this->defaultConnection = $con;
 		}
-		
+
 		$this->connections[$name] = $con;
 	}
-	
+
 	public function getConnection($name = null) {
 		return ($name ? $this->connections[$name] : $this->defaultConnection);
 	}
-	
+
 }
 
 class DataBaseConnection extends PDO {
-	
+
 	public function __construct($dsn, $username = null, $password = null, $options = null) {
 		parent::__construct($dsn, $username, $password, $options);
 		$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 
-	public function fetchAllAssoc($sql, $bind = [], $conName = null) {
+	public function fetchAllAssoc($sql, $bind = []) {
 		$st = $this->prepare($sql);
-		
+
 		foreach ($bind as $key => $value) {
-			$st->bindParam(':'.$key, $value);
+			$st->bindValue(':'.$key, $value);
 		}
-		
+
 		$st->execute();
-		
+
 		return $st->fetchAll(PDO::FETCH_ASSOC);
 	}
-	
+
 }
--- src/php/datamodel/project.php
+++ src/php/datamodel/project.php
@@ -3,25 +3,23 @@
 namespace YP\DataModel;
 
 class Project {
-	
+
 	use \YP\Addin\DataBase;
-	
+
 	public function getProject($projectID) {
 		$records = ORM\ProjectQuery::create()
 			->addSelect('P.projectid', 'ProjectID')
 			->addSelect('PR.title', 'Title')
 			->addSelect('PR.description', 'Description')
 			->joinProjectRevision()
-//			->filterProjectRevisionByStatus('Approved') TODO fix not working
+			->filterProjectRevisionByStatus('Approved')
 			->filterByProjectID($projectID)
 			->setLimit(1)
 			->find();
-			
-		error_log(print_r($records, true));
-		
+
 		return (array_pop($records) ?: null);
 	}
-	
+
 	public function getProjects($offset = null,  $limit = null) {
 		return ORM\ProjectQuery::create()
 			->addSelect('P.projectid', 'ProjectID')
@@ -33,5 +31,5 @@ class Project {
 			->setOffset($offset)
 			->find();
 	}
-		
+
 }
